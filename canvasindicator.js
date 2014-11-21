d27a702c5d8b5cdef38b1d588d05e601f9c5a26b
Sergey Toy <toydestroyer@me.com>
2014-11-21 07:20:55 +0300

    use strict, anonymous function and increase version

--- canvasindicator.js
+++ canvasindicator.js
@@ -1,5 +1,5 @@
 /**
- * Canvas Indicator 1.1
+ * Canvas Indicator 0.1.2
  *
  * Creates an alternative to ajax loader images with the canvas element.
  *
@@ -27,68 +27,75 @@
  *
  **/
 
-function CanvasIndicator(el, opt) {
-    this.ctx = el.getContext("2d");
-    this.currentOffset = 0;
-    var defaults = {
-        bars: 11,
-        innerRadius: 4,
-        size: [2, 5],
-        color: '#fff',
-        fps: 10
-    }
-    if (typeof(opt) == 'object') {
-        defaults.bars = opt.bars ? opt.bars : defaults.bars;
-        defaults.innerRadius = opt.innerRadius ? opt.innerRadius : defaults.innerRadius;
-        defaults.size = opt.size ? opt.size : defaults.size;
-        defaults.color = opt.color ? opt.color : defaults.color;
-        defaults.fps = opt.fps ? opt.fps : defaults.fps;
-    }
-    this.opt = defaults;
-    this.w = this.opt.size[1] + this.opt.innerRadius;
-    el.setAttribute("width", this.w * 2);
-    el.setAttribute("height", this.w * 2);
-    (function nextAnimation(obj) {
-        obj.currentOffset = (obj.currentOffset + 1) % obj.opt.bars;
-        obj.draw(obj.currentOffset);
-        setTimeout(function() {
-            nextAnimation(obj);
-        }, 1000 / obj.opt.fps);
-    })(this);
+;(function(global) {
 
-}
+  'use strict';
 
-CanvasIndicator.prototype.drawBlock = function(barNo) {
-    this.ctx.fillStyle = this.opt.color;
-    this.ctx.globalAlpha = (this.opt.bars + 1 - barNo) / (this.opt.bars + 1);
-    this.ctx.fillRect(-this.opt.size[0] / 2, 0, this.opt.size[0], this.opt.size[1]);
-}
+  function CanvasIndicator(el, opt) {
+      this.ctx = el.getContext("2d");
+      this.currentOffset = 0;
+      var defaults = {
+          bars: 11,
+          innerRadius: 4,
+          size: [2, 5],
+          color: '#fff',
+          fps: 10
+      }
+      if (typeof(opt) == 'object') {
+          defaults.bars = opt.bars ? opt.bars : defaults.bars;
+          defaults.innerRadius = opt.innerRadius ? opt.innerRadius : defaults.innerRadius;
+          defaults.size = opt.size ? opt.size : defaults.size;
+          defaults.color = opt.color ? opt.color : defaults.color;
+          defaults.fps = opt.fps ? opt.fps : defaults.fps;
+      }
+      this.opt = defaults;
+      this.w = this.opt.size[1] + this.opt.innerRadius;
+      el.setAttribute("width", this.w * 2);
+      el.setAttribute("height", this.w * 2);
+      (function nextAnimation(obj) {
+          obj.currentOffset = (obj.currentOffset + 1) % obj.opt.bars;
+          obj.draw(obj.currentOffset);
+          setTimeout(function() {
+              nextAnimation(obj);
+          }, 1000 / obj.opt.fps);
+      })(this);
+  }
 
-CanvasIndicator.prototype.calculatePosition = function(barNo) {
-    angle = 2 * barNo * Math.PI / this.opt.bars;
-    return {
-        y: (this.opt.innerRadius * Math.cos(-angle)),
-        x: (this.opt.innerRadius * Math.sin(-angle)),
-        angle: angle
-    };
-}
+  CanvasIndicator.prototype.drawBlock = function(barNo) {
+      this.ctx.fillStyle = this.opt.color;
+      this.ctx.globalAlpha = (this.opt.bars + 1 - barNo) / (this.opt.bars + 1);
+      this.ctx.fillRect(-this.opt.size[0] / 2, 0, this.opt.size[0], this.opt.size[1]);
+  }
 
-CanvasIndicator.prototype.draw = function(offset) {
-    this.clearFrame();
-    this.ctx.save();
-    this.ctx.translate(this.w, this.w);
-    for (var i = 0; i < this.opt.bars; i++) {
-        var curbar = (offset + i) % this.opt.bars,
-            pos = this.calculatePosition(curbar);
-        this.ctx.save();
-        this.ctx.translate(pos.x, pos.y);
-        this.ctx.rotate(pos.angle);
-        this.drawBlock(i);
-        this.ctx.restore();
-    }
-    this.ctx.restore();
-}
+  CanvasIndicator.prototype.calculatePosition = function(barNo) {
+      var angle = 2 * barNo * Math.PI / this.opt.bars;
+      return {
+          y: (this.opt.innerRadius * Math.cos(-angle)),
+          x: (this.opt.innerRadius * Math.sin(-angle)),
+          angle: angle
+      };
+  }
 
-CanvasIndicator.prototype.clearFrame = function() {
-    this.ctx.clearRect(0, 0, this.ctx.canvas.clientWidth, this.ctx.canvas.clientHeight);
-}
+  CanvasIndicator.prototype.draw = function(offset) {
+      this.clearFrame();
+      this.ctx.save();
+      this.ctx.translate(this.w, this.w);
+      for (var i = 0; i < this.opt.bars; i++) {
+          var curbar = (offset + i) % this.opt.bars,
+              pos = this.calculatePosition(curbar);
+          this.ctx.save();
+          this.ctx.translate(pos.x, pos.y);
+          this.ctx.rotate(pos.angle);
+          this.drawBlock(i);
+          this.ctx.restore();
+      }
+      this.ctx.restore();
+  }
+
+  CanvasIndicator.prototype.clearFrame = function() {
+      this.ctx.clearRect(0, 0, this.ctx.canvas.clientWidth, this.ctx.canvas.clientHeight);
+  }
+
+  global.CanvasIndicator = CanvasIndicator;
+
+})(window);
